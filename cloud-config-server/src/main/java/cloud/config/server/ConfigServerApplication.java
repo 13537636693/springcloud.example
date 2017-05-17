5caf0f6943504685fc67a8b17f7b6f4359d7dbaa
wujiang <29149164@qq.com>
2017-05-17 17:05:15 +0800

    规划配置
    Signed-off-by: wujiang <29149164@qq.com>

--- cloud-config-server/src/main/java/cloud/config/server/ConfigServerApplication.java
+++ cloud-config-server/src/main/java/cloud/config/server/ConfigServerApplication.java
@@ -6,17 +6,26 @@ package cloud.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 //@Configuration
 //@EnableAutoConfiguration
 //@EnableConfigServer
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 @EnableConfigServer
+@Controller
 public class ConfigServerApplication {
+
+	@ResponseBody
+	@RequestMapping(value = "/")
+	String home() {
+		return "config server ,http://localhost:8888/simple-service/dev/master";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
